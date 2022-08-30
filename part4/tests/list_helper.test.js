476b441e7413f85354e208f8119a0ec32f4a29fa
huyenvtn <huyenvtn0210@gmail.com>
2022-08-30 18:00:19 +0700

    feat: part4 init

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -6,3 +6,10 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+test('of empty list is zero', () => {
+    const blogs = []
+
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(1)
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,19 @@
 const dummy = (blogs) => {
     return 1
   }
+
+  const totalLikes = (blogs) => {
+    const total = 0
+    blogs.forEach(element => {
+        if (element.likes) {
+            total +=element.likes
+        }
+    })
+    return total
+  }
+
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
