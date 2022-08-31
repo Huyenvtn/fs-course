8a1e38eddd114b9730d6a7d715a17440e8579c56
huyenvtn <huyenvtn0210@gmail.com>
2022-08-31 17:41:36 +0700

    feat: part4

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,5 +1,9 @@
-const PORT = 3003
-const URI = 'mongodb+srv://huyen:mongodb0210@bloglist.et5raed.mongodb.net/?retryWrites=true&w=majority'
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const URI = process.env.NODE_ENV === 'test' 
+? process.env.TEST_URI
+: process.env.URI
 
 module.exports = {
     PORT,
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -12,8 +12,84 @@ const dummy = (blogs) => {
     return total
   }
 
-  
+  const favoriteBlog = (blogs) => {
+    let favIndex = 0 
+    if (blogs && blogs.length > 0) {
+      let likes = blogs[0].likes
+      for(let i=1; i<blogs.length; i++) {
+        if (blogs[i].likes > likes) {
+          likes = blogs[i].likes
+          favIndex = i
+        }
+      }
+      const obj = {
+        title: blogs[favIndex].title,
+        author: blogs[favIndex].author,
+        likes: blogs[favIndex].likes
+      }
+      return obj
+    }
+    return {}
+  } 
+
+  const mostBlogs = (blogs) => {
+    if (blogs && blogs.length > 0) {
+      let arr = []
+      let mostObj = {
+        author: blogs[0].author,
+        blogs: 1
+      }
+      arr.push(mostObj)
+
+      for(let i = 1; i < blogs.length; i++) {
+        const idx = arr.findIndex(el => el.author === blogs[i].author);
+        if (idx >= 0) {
+          arr[idx].blogs = arr[idx].blogs + 1
+          if (arr[idx].blogs > mostObj.blogs) {
+            mostObj = arr[idx]
+          }
+        } else {
+          var newObj = { author: blogs[i].author, blogs: 1 }
+          arr.push(newObj)
+        }
+      }
+
+      return mostObj
+    }
+    return {}
+  }
+
+  const mostLikes = (blogs) => {
+    if (blogs && blogs.length > 0) {
+      let arr = []
+      let mostObj = {
+        author: blogs[0].author,
+        likes: blogs[0].likes
+      }
+      arr.push(mostObj)
+
+      for(let i = 1; i < blogs.length; i++) {
+        const idx = arr.findIndex(el => el.author === blogs[i].author);
+        if (idx >= 0) {
+          arr[idx].likes = arr[idx].likes + blogs[i].likes
+          if (arr[idx].likes > mostObj.likes) {
+            mostObj = arr[idx]
+          }
+        } else {
+          var newObj = { author: blogs[i].author, likes: blogs[i].likes }
+          arr.push(newObj)
+        }
+      }
+
+      return mostObj
+    }
+    return {}
+  }
+
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
   }
